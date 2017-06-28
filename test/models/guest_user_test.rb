649bc01fe849f04b20613ae77e557c731e4947a1
Mike Foley <foley3@gmail.com>
2017-06-27 20:34:24 -0700

    Use minitest-spec-rails for tests

--- test/models/guest_user_test.rb
+++ test/models/guest_user_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class GuestUserTest < ActiveSupport::TestCase
-  test "is not logged in" do
+  it "is not logged in" do
     guest_user = GuestUser.new
     assert !guest_user.logged_in?
   end
