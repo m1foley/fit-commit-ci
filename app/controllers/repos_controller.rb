26d83b727e8f50dc355f7a5c8ac8c3574a3bc774
Mike Foley <foley3@gmail.com>
2017-08-11 21:22:30 -0700

    Order repos in repos index

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -9,6 +9,9 @@ class ReposController < ApplicationController
   def load_repos_by_organization
     @repos_by_organization = current_user.repos.
       select("repos.*", "memberships.admin AS current_user_admin").
+      order("memberships.admin DESC").
+      order(active: :desc).
+      order("LOWER(name) ASC").
       group_by(&:organization)
   end
 end
