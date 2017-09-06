327a87b683d46716d35afa25dea3a6accc388db2
Mike Foley <foley3@gmail.com>
2017-09-06 13:04:17 -0700

    Add test to verify all fixtures are valid
    I've seen invalid fixtures lead to confusing test failures in other
    projects. This helps prevent that from happening by simply checking that
    all fixtures are valid.

---
+++ test/models/valid_fixtures_test.rb
@@ -0,0 +1,22 @@
+require "test_helper"
+
+class ValidFixturesTest < ActiveSupport::TestCase
+  def format_error(instance)
+    "#{instance.error_messages_formatted} #{instance.inspect}"
+  end
+
+  def test_all_fixtures_are_valid
+    fixture_files = Dir["test/fixtures/*.yml"]
+    assert fixture_files.present?
+    fixture_files.each do |fixture_file|
+      klass = File.basename(fixture_file, ".yml").classify.constantize
+      klass.all.each do |instance|
+        begin
+          assert instance.valid?, format_error(instance)
+        rescue => e
+          raise e, "#{e} #{instance.inspect}"
+        end
+      end
+    end
+  end
+end
